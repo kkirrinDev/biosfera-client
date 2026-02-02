6c8358f71796119f219f2959b297cd304cf1d69c
Сергей Субботин <zarodiny@yandex.ru>
2026-02-02 15:17:53 +1000

    feat: обновляет стили глобальных переменных и компонента Header, добавляет новые классы для кнопок и контейнеров

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,46 @@
 "use client";
+import { useState } from "react";
 import styles from "./style.module.scss";
+import Image from "next/image";
+import { VdsButton, VdsPanel } from "@/app/components";
 
 export default function Header() {
+  const [panel, setPanel] = useState(false);
+  const [panelBtn, setPanelBtn] = useState(true);
   return (
     <header>
-      <h2 className={styles.color}>Header</h2>
-      <p className={styles.text}>Header text paragraph</p>
+      <div className="container">
+        {panel && <VdsPanel setPanel={setPanel} setPanelBtn={setPanelBtn} />}
+
+        <div className={styles.header__inner}>
+          <a href="/">
+            <Image src="/logo.svg" className='dsv-image' alt="logo" width={310} height={66} />
+          </a>
+
+          <div className="flex">
+            <Image src="/icons/pin-icon.svg" className='dsv-image' alt="logo" width={22} height={22} />
+            <p>Пр-т 100-летия Владивостока, 84а</p>
+          </div>
+
+            <a href="tel:+79243388189" className="flex">
+              <Image src="/icons/phone.svg" className='dsv-image' alt="logo" width={22} height={22} />
+              <span>+7 (924) 338-81-89</span>
+            </a>
+
+            <button className={styles.header__button}>Записаться на прием</button>
+
+            <div className={styles.header_social}>
+              <button className={styles.item__button} title="Личный кабинет">
+                <Image src="/icons/account-icon.svg" className='dsv-image' alt="logo" width={22} height={22} />
+              </button>
+              {panelBtn && <VdsButton setPanel={setPanel} setPanelBtn={setPanelBtn} />}
+              <button className={styles.item__button} title="Поиск">
+                <Image src="/icons/search-icon.svg" className='dsv-image' alt="logo" width={22} height={22} />
+              </button>
+            </div>
+        </div>
+
+      </div>
     </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,16 @@
-"use client";
-import { useState } from "react";
 import Image from "next/image";
-import { AnimateElement, Header, VdsButton, VdsPanel } from "./components";
+import { AnimateElement, Header } from "./components";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const [panel, setPanel] = useState(false);
-  const [panelBtn, setPanelBtn] = useState(true);
+
 
   return (
     <main className={styles.main}>
-      {panelBtn && <VdsButton setPanel={setPanel} setPanelBtn={setPanelBtn} />}
-      {panel && <VdsPanel setPanel={setPanel} setPanelBtn={setPanelBtn} />}
-      <Image
-        className={styles.logo}
-        src="/next.svg"
-        alt="Next.js logo"
-        width={100}
-        height={20}
-        priority
-      />
+
+
       <Header />
-
       <h1>Lorem ipsum dolor sit amet consectetur</h1>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id adipisci,
-        temporibus nesciunt cumque deleniti, quas eligendi est esse,
-        reprehenderit aperiam quasi quo nisi accusantium aspernatur soluta
-        doloribus eos nemo molestiae!
-      </p>
-
       <Image
         src="/Item.png"
         alt="VDS"
@@ -39,6 +19,11 @@ export default function Home() {
         className="dsv-image"
         priority
       />
+
+      <p className="text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id adipisci,
+        temporibus nesciunt cumque deleniti, quas eligendi est esse,
+        reprehenderit aperiam quasi quo nisi accusantium aspernatur soluta
+        doloribus eos nemo molestiae!</p>
 
       <AnimateElement element="p">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id adipisci,
